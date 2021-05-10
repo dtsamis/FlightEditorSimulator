5426c71d8f9c3c76a3682c5bf23fe3bd512d6420
dtsamis <dhm.tsamhs@gmail.com>
2021-05-10 23:26:41 +0200

    Initial commit

---
+++ src/main/java/com/example/braiveassignment/BraiveassignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.braiveassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BraiveassignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BraiveassignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/braiveassignment/BraiveassignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.braiveassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BraiveassignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
