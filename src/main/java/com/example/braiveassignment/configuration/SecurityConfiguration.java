ca9099fc3ea472be580cf40b103906996772f3f7
dtsamis <dhm.tsamhs@gmail.com>
2021-05-15 00:33:53 +0200

    Checkedout new branch

--- src/main/java/com/example/braiveassignment/configuration/SecurityConfiguration.java
+++ src/main/java/com/example/braiveassignment/configuration/SecurityConfiguration.java
@@ -23,6 +23,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     }
 
     @Override
+
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests(authorize->{authorize.antMatchers("/api/register*").hasRole("ADMIN");})
                 .authorizeRequests()
