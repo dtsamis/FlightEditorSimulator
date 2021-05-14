44064ef3fce36f5c922da53d9fae6e4da97b86ba
dtsamis <dhm.tsamhs@gmail.com>
2021-05-14 22:54:17 +0200

    Added unit tests for API

---
+++ src/test/java/com/example/braiveassignment/TestUtility.java
@@ -0,0 +1,19 @@
+package com.example.braiveassignment;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+import java.io.IOException;
+
+public class TestUtility {
+
+    public static <T>  Object convertJSONStringToObject(String json, Class<T> objectClass) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+
+        JavaTimeModule module = new JavaTimeModule();
+        mapper.registerModule(module);
+        return mapper.readValue(json, objectClass);
+    }
+}
